7e06bfaf3d9537d26aff60485a725a183140b060
rugger_sr <rugger.sr@gmail.com>
2019-03-02 17:02:24 +0900

    remove comment at ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,14 +6,6 @@ module ApplicationHelper
       I18n.t("activerecord.attributes.#{model_name}.#{attribute_name}")
     end
   end
-  
-  # def simple_time(time)
-  #   if time.nil?
-  #     return ""
-  #   else
-  #     return time.strftime("%Y-%m-%d %H:%M:%S")
-  #   end
-  # end
 
   def simple_time(time)
     time&.strftime("%Y-%m-%d %H:%M:%S")
